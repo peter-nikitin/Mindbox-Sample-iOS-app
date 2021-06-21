3a2dc0778b4ab4f90206a31b60a1ae9270b4c74c
Petr <nikitin.p.94@gmail.com>
2021-06-21 14:16:38 +0300

    Initial Commit

---
+++ Mindbox Sample App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mindbox Sample App
+//
+//  Created by Никитин Петр on 21.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
